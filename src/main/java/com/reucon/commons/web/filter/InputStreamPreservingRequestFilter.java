815bcde67208cee8bd1890ab8b97a82fd5082a88
Thomas Kruse <thomas.kruse@reucon.com>
2015-04-08 22:42:03 +0200

    Use spring http request wrapper to support POST params

--- src/main/java/com/reucon/commons/web/filter/InputStreamPreservingRequestFilter.java
+++ src/main/java/com/reucon/commons/web/filter/InputStreamPreservingRequestFilter.java
@@ -1,6 +1,5 @@
 package com.reucon.commons.web.filter;
 
-import com.reucon.commons.web.request.CachedHttpRequestWrapper;
 import java.io.IOException;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -9,6 +8,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.util.ContentCachingRequestWrapper;
 
 public class InputStreamPreservingRequestFilter extends GenericFilterBean
 {
@@ -23,9 +23,9 @@ public class InputStreamPreservingRequestFilter extends GenericFilterBean
         final HttpServletRequest request = (HttpServletRequest) req;
         final HttpServletResponse response = (HttpServletResponse) res;
         
-        final CachedHttpRequestWrapper wrappedRequest = new CachedHttpRequestWrapper(request);
+        final ContentCachingRequestWrapper wrappedRequest = new ContentCachingRequestWrapper(request);
         
-        wrappedRequest.setAttribute(REQUEST_ATTRIBUTE, wrappedRequest.getBytes());
+        wrappedRequest.setAttribute(REQUEST_ATTRIBUTE, wrappedRequest.getContentAsByteArray());
 
         chain.doFilter(wrappedRequest, response);
     }
